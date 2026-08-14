256afea95eea5946ca6af1759490a915c31ede04
Silva4516 <silvaduartealice30@gmail.com>
2026-08-13 21:09:18 -0300

    Add files via upload

---
+++ src/flamingo/aprendendo/introducao/Comentario.java
@@ -0,0 +1,15 @@
+package flamingo.aprendendo.introducao;
+
+/**aprendendo a fazer comentario*/
+public class Comentario {
+    //Aula com Bigas
+    /* Aula
+        com
+        Bigas
+     */
+    public static void main(String[] args) {
+        System.out.println("Comentario");
+    }
+
+
+     }
