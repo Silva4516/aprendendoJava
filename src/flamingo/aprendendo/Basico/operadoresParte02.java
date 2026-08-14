276a8492a36c617b120dc334adc4cc160c9996c6
Silva4516 <silvaduartealice30@gmail.com>
2026-08-13 21:07:22 -0300

    Add files via upload

---
+++ src/flamingo/aprendendo/Basico/operadoresParte02.java
@@ -0,0 +1,33 @@
+package flamingo.aprendendo.Basico;
+
+public class operadoresParte02 {
+    /*
+    * < menor que
+    * > maior que
+    * <= menor ou igual
+    * >= maior ou igual
+    * == igual a
+    * != diferente de
+     */
+
+    public static void main(String[] args) {
+        int numero01  = 10;
+        int numero02 = 20;
+
+
+        System.out.println( numero01 < numero02 );
+        System.out.println(numero01 > numero02);
+        System.out.println(numero01<= numero02);
+        System.out.println(numero01>= numero02);
+        System.out.println(numero01<= numero02);
+        System.out.println(numero01 == numero02);
+        System.out.println(numero01 != numero02);
+
+
+
+
+
+
+
+    }
+}
---
+++ src/flamingo/aprendendo/introducao/TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package flamingo.aprendendo.introducao;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        // byte : -128 -127
+        //short : -32.768 a 32.767
+        byte idade= 18;
+        int municipio = 100000;
+        System.out.println("Minha idade é " + idade);
+        System.out.println("São paulo (SP): Mais de" + municipio + " milhoes de moradores.");
+    }
+}
